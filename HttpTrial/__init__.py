1e95bea8a38aaf85313d83d88e6aab02064c98ca
adefemi171 <adefemi171@gmail.com>
2020-12-30 18:31:32 -0500

    worked on the route

--- HttpTrial/__init__.py
+++ HttpTrial/__init__.py
@@ -2,19 +2,8 @@ import logging
 import json
 
 import azure.functions as func
-def fetch_meal_data(id: int) -> any:
-    '''
-    Fetch specific data based on Meals JSON
-    '''
-    with open("meal.json", "r") as json_file:
-            data = json.load(json_file)
 
-    # with open("meal.json") as json_file:
-    #     dt = json.load(json_file)
-    #     data = json.dumps(dt, indent=4, sort_keys=True)
-    mealID = id - 1
-
-    return str(data[mealID])
+from .fetchData import fetch_meal_data, fetch_mul_meal_data
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
@@ -31,15 +20,8 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     if name:
         return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
     else:
-        # data = str(json.load(open("meal.json")))
-        # loading data directly from json file
-        # with open("meal.json", "r") as json_file:
-        #     data = str(json.load(json_file))
-        # return func.HttpResponse(data[:15])
+        user = req.params.get('id')
+        if user == None:
+            return func.HttpResponse("no ID provided. use http://localhost:7071/api/HttpTrial/?id=1")
 
-        # with open("meal.json", "r") as json_file:
-        #     data = json.load(json_file)
-        # return func.HttpResponse(str(data[0]))
-
-        #Using fetch_meal_data function to fetch data
-        return func.HttpResponse(fetch_meal_data(1))
+        return func.HttpResponse(json.dumps(fetch_mul_meal_data(int(user))))
---
+++ HttpTrial/fetchData.py
@@ -0,0 +1,26 @@
+import json
+
+
+def fetch_meal_data(id: int) -> any:
+    '''
+    Fetch specific data based on Meals JSON
+    '''
+    with open("meal.json", "r") as json_file:
+            data = json.load(json_file)
+
+    
+    mealID = id - 1
+
+    return str(data[mealID])
+
+
+
+def fetch_mul_meal_data(valRange: int):
+    ids = []
+    for i in range(1, valRange + 1):
+        id = fetch_meal_data(i)
+        ids.append(id)
+
+    return ids 
+
+
