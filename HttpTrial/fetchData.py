25bd521c8339742e5c3400f21a3a4229edd976fb
[InnCreator] <[engbethany4@gmail.com]>
2021-01-03 20:48:01 +0300

    func to extract all data

--- HttpTrial/fetchData.py
+++ HttpTrial/fetchData.py
@@ -5,13 +5,19 @@ def fetch_meal_data(id: int) -> any:
     '''
     Fetch specific data based on Meals JSON
     '''
-    with open("meal.json", "r") as json_file:
+    with open("KenyanMeals.json", "r") as json_file:
             data = json.load(json_file)
 
     
     mealID = id - 1
 
     return str(data[mealID])
+
+def fetch_json_file(jsonFile: str):
+    with open(jsonFile, "r") as json_file:
+            data = json.load(json_file)
+
+    return data
 
 
 
